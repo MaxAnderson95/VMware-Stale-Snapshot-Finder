b58cc95413cfefc170818c57e60f541a7fb68d89
Max Anderson <cctpmax@gmail.com>
2018-06-20 19:59:08 -0400

    Added Find-PowerCLI and a module manifest

---
+++ VMware-Stale-Snapshot-Finder/Private/Find-PowerCLI.ps1
@@ -0,0 +1,30 @@
+Function Find-PowerCLI {
+
+    <#
+
+        .SYNOPSIS
+        Returns wether PowerCLI is installed
+
+        .OUTPUTS
+        System.Boolean
+
+    #>
+
+    [CmdletBinding()]
+
+    #Get the module
+    $PowerCLI = Get-Module -Name VMware.PowerCLI
+
+    #If a module is found, return true, otherwise return false
+    If ($PowerCLI -eq $Null) {
+        Return $False
+
+    }
+
+    Else {
+
+        Return $True
+        
+    }
+
+}
