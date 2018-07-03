fd846bf5c5db5fbdbcdc42c33ba56b53676baad0
unknown <cctpmax@gmail.com>
2018-07-03 18:26:19 -0400

    Added a type cast to the VM parameter using the generic object type and allowing an array of VMs

--- VMware-Stale-Snapshot-Finder/Public/Get-VMStaleSnapshot.ps1
+++ VMware-Stale-Snapshot-Finder/Public/Get-VMStaleSnapshot.ps1
@@ -5,7 +5,7 @@ Function Get-VMStaleSnapshot {
 
         [Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,ParameterSetName='DateTimeObject')]
         [Parameter(ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,ParameterSetName='Days')]
-        $VM,
+        [object[]]$VM,
 
         [Parameter(Mandatory=$True,ParameterSetName='DateTimeObject')]
         [DateTime]$Date,
