32a42b329bef47c3c1ae13bd1d6475c2c5353998
unknown <cctpmax@gmail.com>
2018-07-03 21:24:56 -0400

    Added verbose output to Find-PowerCLI private function

--- VMware-Stale-Snapshot-Finder/Private/Find-PowerCLI.ps1
+++ VMware-Stale-Snapshot-Finder/Private/Find-PowerCLI.ps1
@@ -10,20 +10,21 @@ Function Find-PowerCLI {
 
     #>
 
-    [CmdletBinding()]
-
     #Get the module
+    Write-Verbose "Querying for the PowerCLI module to see if it is installed."
     $PowerCLI = Get-Module -Name VMware.PowerCLI -ListAvailable
 
     #If a module is found, return true, otherwise return false
     If ($PowerCLI -eq $Null) {
         
+        Write-Verbose "PowerCLI module not installed on the system."
         Return $False
 
     }
 
     Else {
 
+        Write-Verbose "PowerCLI module installed on the system."
         Return $True
         
     }
